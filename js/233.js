5444ff170fa0ebd6e658abbdcc288b8e2efac765
Pisces000221 <1786762946@qq.com>
2015-02-13 14:34:27 +0800

    Simplify code by removing 'remove' operations and use a duration instead

--- js/233.js
+++ js/233.js
@@ -7,11 +7,9 @@
 
   var js233 = (function () {
     var comments = [[], [], []];
-    // EV_SLIDING_RM is not necessarily used.
-    // Sliding comments can be removed when it reaches the left border.
-    var EV_SLIDING_ADD = 1, EV_SLIDING_RM = 2;
-    var EV_TOP_STICKY_ADD = 3, EV_TOP_STICKY_RM = 4;
-    var EV_BOTTOM_STICKY_ADD = 5, EV_BOTTOM_STICKY_RM = 6;
+    var CMT_SLIDING = 1;
+    var CMT_TOP_STICKY = 2;
+    var CMT_BOTTOM_STICKY = 3;
 
     var SLIDE_MIN_TIME = 10, SLIDE_MAX_TIME = 25;
     var STICKY_MIN_TIME = 2, STICKY_MAX_TIME = 3;
@@ -37,38 +35,37 @@
       if (j > l) sort(a, l, j);
     }
 
-    function stickyOperations(cmtList, oprList, isSliding, minDur, maxDur, addEvent, rmEvent) {
+    function stickyOperations(cmtList, oprList, isSliding, minDur, maxDur, opType) {
       sort(cmtList);
-      var posUsage = [], i, j, cur, found, add, rm, dur, useTime;
+      var posUsage = [], i, j, cur, found, op, dur, useUntil;
       // Iterate through each comment.
       for (i = 0; i < cmtList.length; i++) {
         cur = cmtList[i];
         found = false;
-        add = {type: addEvent, id: i, time: cur[0], text: cur[1], colour: cur[2]};
-        dur = cur[0] + Math.random() * (maxDur - minDur) + minDur;
-        useTime = cur.time + dur;
-        rm = {type: rmEvent, id: i, time: dur};
+        dur = Math.random() * (maxDur - minDur) + minDur;
+        op = {type: opType,
+            time: cur[0], duration: dur, text: cur[1], colour: cur[2]};
+        useUntil = cur[0] + dur;
         // If sliding, pass the duration to the operator so it can know the speed.
         // Also, sliding comments only uses the line for (0.3 * duration) seconds
         // so that multiple comments can be in one line - and THERE COMES DANMAKU!!
         if (isSliding) {
-          add.duration = dur;
-          useTime = cur.time + dur * 0.3;
+          op.duration = dur;
+          useUntil = cur[0] + dur * 0.3;
         }
         // Find a place to put the comment.
         for (j = 0; j < posUsage.length; j++) if (posUsage[j] < cur[0]) {
           // Found that line #i is available. Put it there.
-          add.line = j;
-          posUsage[j] = rm.time;
+          op.line = j;
+          posUsage[j] = useUntil;
           found = true; break;
         }
         if (!found) {
           // No current line is available. Allocate a new one.
-          add.line = posUsage.length;
-          posUsage.push(rm.time);
+          op.line = posUsage.length;
+          posUsage.push(useUntil);
         }
-        oprList.push(add);
-        oprList.push(rm);
+        oprList.push(op);
       }
     }
 
@@ -76,25 +73,22 @@
       var r = [];
       // Sliding comments (type 0)
       stickyOperations(comments[0], r, true,
-        SLIDE_MIN_TIME, SLIDE_MAX_TIME, EV_SLIDING_ADD, EV_SLIDING_RM);
+        SLIDE_MIN_TIME, SLIDE_MAX_TIME, CMT_SLIDING);
       // Top-sticky comments (type 1)
       stickyOperations(comments[1], r, false,
-        STICKY_MIN_TIME, STICKY_MAX_TIME, EV_TOP_STICKY_ADD, EV_TOP_STICKY_RM);
+        STICKY_MIN_TIME, STICKY_MAX_TIME, CMT_TOP_STICKY);
       // Bottom-sticky comments (type 2)
       stickyOperations(comments[2], r, false,
-        STICKY_MIN_TIME, STICKY_MAX_TIME, EV_BOTTOM_STICKY_ADD, EV_BOTTOM_STICKY_RM);
+        STICKY_MIN_TIME, STICKY_MAX_TIME, CMT_BOTTOM_STICKY);
       return r;
     }
 
     return {
       addComment: addComment,
       getAll: getAll,
-      EV_SLIDING_ADD: EV_SLIDING_ADD,
-      EV_SLIDING_RM: EV_SLIDING_RM,
-      EV_TOP_STICKY_ADD: EV_TOP_STICKY_ADD,
-      EV_TOP_STICKY_RM: EV_TOP_STICKY_RM,
-      EV_BOTTOM_STICKY_ADD: EV_BOTTOM_STICKY_ADD,
-      EV_BOTTOM_STICKY_RM: EV_BOTTOM_STICKY_RM
+      CMT_SLIDING: CMT_SLIDING,
+      CMT_TOP_STICKY: CMT_TOP_STICKY,
+      CMT_BOTTOM_STICKY: CMT_BOTTOM_STICKY
     };
   }());
 
